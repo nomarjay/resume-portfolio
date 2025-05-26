01f1d15eaa8b7d394f5c27006a21c35132ab0f36
Jordan King <105494965+nomarjay@users.noreply.github.com>
2025-05-26 19:45:16 -0400

    updated css for mobile screen sizes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".toggleBtn").forEach((button) => {
+  button.addEventListener("click", () => {
+    const wrapper = button.closest(".content"); 
+    const toggleContent = wrapper.querySelector(".toggleContent");
+
+    const isHidden = toggleContent.classList.toggle("hide");
+    button.textContent = isHidden ? "More" : "Less";
+  });
+});
